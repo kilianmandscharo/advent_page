1d312177be99d1fca267873bb64a82213a678ca1
Kilian <kilianmandscharo@gmail.com>
2023-12-01 15:20:49 +0100

    Initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,90 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import dayjs from "dayjs";
+import dayOfYear from "dayjs/plugin/dayOfYear";
+import customParseFormat from "dayjs/plugin/customParseFormat";
 
-const inter = Inter({ subsets: ['latin'] })
+import { parse, Node, HTMLElement } from "node-html-parser";
 
-export default function Home() {
+dayjs.extend(dayOfYear);
+dayjs.extend(customParseFormat);
+
+const CALENDAR_IDS = ["2190", "2172", "2238", "2221", "2248"];
+
+interface Item {
+  id: string;
+  won: boolean;
+}
+
+interface Props {
+  items: Item[];
+}
+
+export default function Home({ items }: Props) {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main>
+      <h1 className="text-6xl mb-4 text-yellow-500">Advent</h1>
+      <h2 className="text-4xl mb-4">{`Gewinner: (${dayjs().format(
+        "DD.MM.YYYY"
+      )})`}</h2>
+      {items.length === 0 && <p className="italic">Keine Gewinnzahlen</p>}
+      <div className="grid grid-cols-2 mb-4 w-28">
+        {items.map((item) => (
+          <p
+            className={item.won ? "text-green-500" : "text-red-500"}
+            key={item.id}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+            {item.id}
+          </p>
+        ))}
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <a
+        className="underline"
+        href="https://schwabmuenchen-lechfeld-buchloe.lions.de/buchloe"
+      >
+        Zum Gewinnspiel
+      </a>
     </main>
-  )
+  );
+}
+
+export async function getStaticProps() {
+  const res = await fetch(
+    "https://schwabmuenchen-lechfeld-buchloe.lions.de/buchloe"
+  );
+  const htmlString = await res.text();
+  const root = parse(htmlString);
+  const ids = traverse(root, []);
+  const items = ids.map((id) => ({ id, won: CALENDAR_IDS.includes(id) }));
+
+  return {
+    props: {
+      items,
+    },
+    revalidate: 60 * 60,
+  };
+}
+
+function traverse(node: Node, ids: string[]) {
+  if (node.nodeType === 1) {
+    const el = node as HTMLElement;
+    if (el.rawTagName === "tr" && el.childNodes.length > 0) {
+      const dateString = el.childNodes[0].innerText;
+      const date = dayjs(dateString, "DD.MM.YYYY");
+      const now = dayjs().add(1, "h");
+      if (
+        date.isValid() &&
+        date.dayOfYear() === now.dayOfYear() &&
+        el.childNodes.length > 3
+      ) {
+        ids.push(
+          ...el.childNodes[3].innerText.split(",").map((text) => text.trim())
+        );
+      }
+    }
+  }
+
+  for (const child of node.childNodes) {
+    ids = traverse(child, ids);
+  }
+
+  return ids;
 }
